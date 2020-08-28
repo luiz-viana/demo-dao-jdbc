d13f1a9e0596e1bc520f71b238a4926b559535d1
luiz-viana <luiz9505@hotmail.com>
2020-08-28 13:15:59 -0300

    departmentDao DeleteById implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -12,13 +12,18 @@ public class Program2 {
 		
 		Scanner sc = new Scanner (System.in);
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
-	
 		
 		System.out.println("=== TEST 1 - INSERT ===");
 		Department dp = new Department(null, "Music");
 		departmentDao.insert(dp);
 		
+		System.out.println("=== TEST 2 - UPDATE ===");
+		dp.setId(5);
+		dp.setName("food");
+		departmentDao.update(dp);
 		
+		System.out.println("=== TEST 3 - DELETE BY ID ===");
+		departmentDao.deleteById(6);
 		
 		
 		
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -51,20 +51,58 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 		finally {
 			DB.closeStatement(st);
 		}
-		
-		
-		
 	}
 
+	
 	@Override
 	public void update(Department dp) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"UPDATE department "
+					+"SET Name = ? "
+					+"WHERE Id = ?",
+					Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, dp.getName());
+			st.setInt(2, dp.getId());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if (rowsAffected == 0) {
+				throw new DbException("No rows Updated! ");
+			}
+			
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		
+		try {
+			st = conn.prepareStatement("DELETE FROM department WHERE Id = ?");
+			st.setInt(1, id);
+			
+			int rowsAffected = st.executeUpdate();
+			if (rowsAffected == 0) {
+				throw new DbException("NO rows Affected!");
+			}	
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
